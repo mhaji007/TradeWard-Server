88026b55fc8d113a206c0d1238fd77b8cf785fbc
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-02-25 20:53:20 -0500

    Add reference of the currently logged-in user upon product creation

--- controllers/product.js
+++ controllers/product.js
@@ -6,6 +6,8 @@ const APIFeatures = require("../utils/apiFeatures")
 // Create a new product
 
 exports.newProduct = catchAsyncErros(async (req, res, next) => {
+
+  req.body.user = req.user.id
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
